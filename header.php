54153bd14a6284e302576b78ab09b37376badd73
Thumira2010 <146361091+Thumira2010@users.noreply.github.com>
2023-10-15 19:45:27 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header-style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="icon" href="favicon-16.png" type="x-icon">
+</head>
+
+<body>
+
+
+
+    <header>
+
+        <input type="checkbox" id="check">
+        <div class="imgflex">
+            <img class="img" src="Logo.png" alt="">
+            <img class="swds" src="SWDS.png" alt="">
+        </div>
+        <label>SWDS</label>
+
+        <nav>
+            <ul class="nav-links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Competitions</a></li>
+            </ul>
+        </nav>
+        <a href="#"><button>Login<i class="bi bi-box-arrow-in-right"></i></button></a>
+
+
+    </header>
+    <hr>
+
+    <i id="bi" class="bi bi-list"></i>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>SWDS</title>
+</head>
+
+<body>
+    <?php
+    include("header.php");
+    ?>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <img src="SWDS.png" alt="" style="width: 30%;margin-left:500px;margin-top:50px">
+
+
+
+
+
+</body>
+
+</html>
